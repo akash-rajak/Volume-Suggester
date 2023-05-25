1916e4e85866f45e9bb5d449bf22d7b3bf79b9bb
akash-rajak <aakashrajak02@gmail.com>
2023-05-25 10:37:19 +0530

    Updated code and ran output

--- Volume Suggester.py
+++ Volume Suggester.py
@@ -22,7 +22,7 @@ from pathlib import Path
 file = "" ## for selected file path
 wav_file = "" ## for converted wav path
 plot_path = "" ## path to store the graph plots
-new_dir = ""
+new_dir = "" ## to create a new directory of output
 paused = False ## to keep track of audio being paused
 stopped = False ## to keep track of audio being stopped
 
@@ -42,8 +42,8 @@ def mp3towav():
     print("\nConverted .mp3 to .wav file and saved to same location from where .mp3 file selected...")
     print("Created : " + base_file_name + "_wav" + ".wav")
     # print(wav_file)
-    # subprocess.call(['C:/Users/MAQ/Path_programs/ffmpeg.exe', '-i', file, wav_file])
-    subprocess.call(['C:/Users/aakas/PATH_Programs/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe', '-i', file, wav_file])
+    subprocess.call(['C:/Users/MAQ/Path_programs/ffmpeg.exe', '-i', file, wav_file])
+    # subprocess.call(['C:/Users/aakas/PATH_Programs/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe', '-i', file, wav_file])
 
     ## creating folder for saving output plot
     new_dir = pathlib.Path(dir_name, base_file_name + " - Plot")
